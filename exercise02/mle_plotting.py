bf0749edcca405a872f1f3aeb49bc8e8e8730b3c
Anian Ruoss <anian.ruoss@gmail.com>
2019-03-10 15:38:50 +0100

    Estimate and plot most likely Poisson distribution

--- exercise02/mle_plotting.py
+++ exercise02/mle_plotting.py
@@ -1,9 +1,10 @@
-from math import exp, gamma, isclose, log, pi
+from functools import reduce
+from math import exp, isclose, log, pi
 from os import makedirs, path
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import stats
+from scipy import special
 
 working_dir = path.dirname(path.abspath(__file__))
 makedirs(path.join(working_dir, 'plots'), exist_ok=True)
@@ -32,44 +33,44 @@ def hist(x_array, n_bins, normalize=True):
 
 num_bins = 100
 counts, bins = hist(data, num_bins, normalize=False)
-plt.bar(bins, counts, width=0.5, align='edge')
+plt.bar(bins, counts, width=0.5, align='edge', color='gray')
 plt.savefig(path.join(working_dir, 'plots/hist.eps'), bbox_inches='tight')
 plt.close()
 
 counts, bins = hist(data, num_bins, normalize=True)
-plt.bar(bins, counts, width=0.5, align='edge')
+plt.bar(bins, counts, width=0.5, align='edge', color='gray')
 plt.savefig(
     path.join(working_dir, 'plots/hist_normalized.eps'), bbox_inches='tight'
 )
 
 
-def beta_likelihood(x, alpha, beta):
-    b_alpha_beta = gamma(alpha) * gamma(beta) / gamma(alpha + beta)
+def poisson_likelihood(x, lambda_):
     n = x.shape[0]
+    lambda_x = reduce(
+        lambda y, z: y * z, (lambda_ ** x).tolist()
+    )
+    x_factorial = reduce(
+        lambda y, z: y * z, special.factorial(x, exact=True).tolist()
+    )
 
-    x_alpha = np.prod(x ** (alpha - 1))
-    x_beta = np.prod((1. - x) ** (beta - 1))
-
-    return (x_alpha * x_beta) / (b_alpha_beta ** n)
+    return exp(- lambda_ * n) * lambda_x / x_factorial
 
 
-def beta_log_likelihood(x, alpha, beta):
-    log_b_alpha_beta = log(gamma(alpha) * gamma(beta) / gamma(alpha + beta))
+def poisson_log_likelihood(x, lambda_):
     n = x.shape[0]
+    log_lambda_x = log(lambda_) * np.sum(x)
+    log_x_factorial = np.sum(np.log(special.factorial(x, exact=True)))
 
-    log_x_alpha = (alpha - 1) * np.sum(log(x))
-    log_x_beta = (beta - 1) * np.sum(log(1. - x))
-
-    return log_x_alpha + log_x_beta - n * log_b_alpha_beta
+    return (- lambda_ * n) + log_lambda_x - log_x_factorial
 
 
-# TODO: Subtask 6. - Calculate MLE(s) of the params from the data
+# Poisson MLE
+lambda_hat = np.mean(data)
 
 
 def gaussian_likelihood(x, mu, var):
     n = x.shape[0]
     normalization_factor = (2. * pi * var) ** (-.5 * n)
-
     x_minus_mu_squared = np.sum((x - mu) ** 2)
 
     return normalization_factor * exp(- x_minus_mu_squared / (2. * var))
@@ -78,13 +79,12 @@ def gaussian_likelihood(x, mu, var):
 def gaussian_log_likelihood(x, mu, var):
     n = x.shape[0]
     log_normalization_factor = (-.5 * n) * log(2. * pi * var)
-
     x_minus_mu_squared = np.sum((x - mu) ** 2)
 
     return log_normalization_factor - x_minus_mu_squared / (2. * var)
 
 
-# gaussian MLE
+# Gaussian MLE
 mu_hat = np.mean(data)
 var_hat = np.var(data)
 
@@ -96,34 +96,36 @@ assert np.equal(
     gaussian_likelihood(data, mu_hat, var_hat)
 )
 
-# TODO : Subtask 7. - Uncomment and evaluate likelihoods
-"""
-assert np.equal(
-    exp(beta_log_likelihood()),
-    beta_likelihood()
-)
-"""
+print('Poisson')
+print('Lambda (MLE): \t\t', lambda_hat)
+try:
+    print('Likelihood: \t\t', poisson_likelihood(data, lambda_hat))
+except OverflowError:
+    print('Likelihood: \t\t', exp(poisson_log_likelihood(data, lambda_hat)))
+print('Log-Likelihood: \t', poisson_log_likelihood(data, lambda_hat))
+print()
 
-# TODO : Subtask 8. - Plot the density of your distribution
+print('Gaussian')
+print('Mu (MLE): \t\t', mu_hat)
+print('Var (MLE): \t\t', var_hat)
+print('Likelihood: \t\t', gaussian_likelihood(data, mu_hat, var_hat))
+print('Log-Likelihood: \t', gaussian_log_likelihood(data, mu_hat, var_hat))
+
 # plot gaussian distribution
-data_range = np.arange(0, np.max(data), step=0.1)
-gaussian_density = list(
+data_range = np.arange(0, np.max(data), step=1)
+gaussian_densities = list(
     map(
         lambda x: gaussian_likelihood(np.array([x]), mu_hat, var_hat),
         data_range
     )
 )
-plt.plot(data_range, gaussian_density, c='k', label='Gaussian', linewidth=3)
+plt.plot(data_range, gaussian_densities, c='g', label='Gaussian', linewidth=3)
 
-# plot beta distribution (out of curiosity)
-alpha, beta, loc, scale = stats.beta.fit(data)
-plt.plot(
-    data_range,
-    stats.beta.pdf(data_range, alpha, beta, loc=loc, scale=scale),
-    c='r',
-    label='Beta',
-    linewidth=3
+# plot poisson distribution
+poisson_densities = list(
+    map(lambda x: poisson_likelihood(np.array([x]), lambda_hat), data_range)
 )
+plt.plot(data_range, poisson_densities, c='r', label='Poisson', linewidth=3)
 
 plt.legend()
 plt.savefig(
