7d42c8cbd521d48bc07ed47a5872cd7424f4bb94
Anian Ruoss <anian.ruoss@gmail.com>
2019-03-30 12:05:55 +0100

    Compute likelihood and posterior with Korali

--- exercise03/task2/task2a.cpp
+++ exercise03/task2/task2a.cpp
@@ -1,30 +1,89 @@
 #include "model/heat2d.hpp"
 #include "korali.h"
 
-int main(int argc, char* argv[])
-{
- // Loading temperature measurement data
 
- FILE* dataFile = fopen("data.in", "r");
- fscanf(dataFile, "%lu", &p.nPoints);
+int main(int argc, char* argv[]) {
+	// Loading temperature measurement data
 
- p.xPos    = (double*) calloc (sizeof(double), p.nPoints);
- p.yPos    = (double*) calloc (sizeof(double), p.nPoints);
- p.refTemp = (double*) calloc (sizeof(double), p.nPoints);
+	FILE* dataFile = fopen("data.in", "r");
+	fscanf(dataFile, "%lu", &p.nPoints);
 
- for (int i = 0; i < p.nPoints; i++)
- {
-  fscanf(dataFile, "%le ", &p.xPos[i]);
-  fscanf(dataFile, "%le ", &p.yPos[i]);
-  fscanf(dataFile, "%le ", &p.refTemp[i]);
- }
+	p.xPos    = (double*) calloc (sizeof(double), p.nPoints);
+	p.yPos    = (double*) calloc (sizeof(double), p.nPoints);
+	p.refTemp = (double*) calloc (sizeof(double), p.nPoints);
 
- // How many candles will we simulate?
- // p.nCandles = 1; // 1-Candle Model - Requires 2 parameters (PosX, PosY)
- // p.nCandles = 2; // 2-Candle Model - Requires 4 parameters (PosX, PosY)x2
- // p.nCandles = 3; // 3-Candle Model - Requires 6 parameters (PosX, PosY)x3
+	for (int i = 0; i < p.nPoints; i++)
+	{
+		fscanf(dataFile, "%le ", &p.xPos[i]);
+		fscanf(dataFile, "%le ", &p.yPos[i]);
+		fscanf(dataFile, "%le ", &p.refTemp[i]);
+	}
 
- // Start configuring the Problem and the Korali Engine
+	// Start configuring the Problem and the Korali Engine
+	Korali::Problem::Likelihood problem(heat2DSolver);
 
- return 0;
+	// 1-Candle Model
+	/*
+	p.nCandles = 1;
+	
+	Korali::Parameter::Uniform pos_x_1("pos_x_1", 0.0, 1.0);
+	Korali::Parameter::Uniform pos_y_1("pos_y_1", 0.0, 1.0);
+
+	problem.addParameter(&pos_x_1);
+	problem.addParameter(&pos_y_1);
+	*/
+
+	// 2-Candle Model
+	/*
+	p.nCandles = 2; 
+
+	Korali::Parameter::Uniform pos_x_1("pos_x_1", 0.0, 0.5);
+	Korali::Parameter::Uniform pos_y_1("pos_y_1", 0.0, 1.0);
+	Korali::Parameter::Uniform pos_x_2("pos_x_2", 0.5, 1.0);
+	Korali::Parameter::Uniform pos_y_2("pos_y_2", 0.0, 1.0);
+
+	problem.addParameter(&pos_x_1);
+	problem.addParameter(&pos_y_1);
+	problem.addParameter(&pos_x_2);
+	problem.addParameter(&pos_y_2);
+	*/
+
+	// 3-Candle Model
+	p.nCandles = 3;
+
+	Korali::Parameter::Uniform pos_x_1("pos_x_1", 0.0, 0.5);
+	Korali::Parameter::Uniform pos_y_1("pos_y_1", 0.0, 1.0);
+	Korali::Parameter::Uniform pos_x_2("pos_x_2", 0.5, 1.0);
+	Korali::Parameter::Uniform pos_y_2("pos_y_2", 0.0, 1.0);
+	Korali::Parameter::Uniform pos_x_3("pos_x_3", 0.5, 1.0);
+	Korali::Parameter::Uniform pos_y_3("pos_y_3", 0.0, 1.0);
+
+	problem.addParameter(&pos_x_1);
+	problem.addParameter(&pos_y_1);
+	problem.addParameter(&pos_x_2);
+	problem.addParameter(&pos_y_2);
+	problem.addParameter(&pos_x_3);
+	problem.addParameter(&pos_y_3);
+
+	problem.setReferenceData(p.nPoints, p.refTemp);
+
+	// Sample distribution
+	/*
+	Korali::Solver::TMCMC solver(&problem);
+
+	solver.setPopulationSize(10000);
+	solver.setCovarianceScaling(0.2);
+	*/
+	
+	// Find maximum parameters
+	Korali::Solver::CMAES solver(&problem);
+
+	solver.setStopMinDeltaX(1e-11);
+	solver.setPopulationSize(64);
+	solver.setMaxGenerations(1000);
+
+	solver.run();
+
+	return 0;
 }
+
--- exercise03/task2/task2b.cpp
+++ exercise03/task2/task2b.cpp
@@ -1,28 +1,68 @@
 #include "model/heat2d.hpp"
 #include "korali.h"
 
-int main(int argc, char* argv[])
-{
- // Loading temperature measurement data
 
- FILE* dataFile = fopen("data.in", "r");
- fscanf(dataFile, "%lu", &p.nPoints);
+int main(int argc, char* argv[]) {
+	// Loading temperature measurement data
 
- p.xPos    = (double*) calloc (sizeof(double), p.nPoints);
- p.yPos    = (double*) calloc (sizeof(double), p.nPoints);
- p.refTemp = (double*) calloc (sizeof(double), p.nPoints);
+	FILE* dataFile = fopen("data.in", "r");
+	fscanf(dataFile, "%lu", &p.nPoints);
 
- for (int i = 0; i < p.nPoints; i++)
- {
-  fscanf(dataFile, "%le ", &p.xPos[i]);
-  fscanf(dataFile, "%le ", &p.yPos[i]);
-  fscanf(dataFile, "%le ", &p.refTemp[i]);
- }
+	p.xPos    = (double*) calloc (sizeof(double), p.nPoints);
+	p.yPos    = (double*) calloc (sizeof(double), p.nPoints);
+	p.refTemp = (double*) calloc (sizeof(double), p.nPoints);
 
- // How many candles will we simulate?
- // p.nCandles = 1; // 1-Candle Model - Requires 2 parameters (PosX, PosY)
- // p.nCandles = 2; // 2-Candle Model - Requires 4 parameters (PosX, PosY)x2
- //  p.nCandles = 3; // 3-Candle Model - Requires 6 parameters (PosX, PosY)x3
+	for (int i = 0; i < p.nPoints; i++)
+	{
+		fscanf(dataFile, "%le ", &p.xPos[i]);
+		fscanf(dataFile, "%le ", &p.yPos[i]);
+		fscanf(dataFile, "%le ", &p.refTemp[i]);
+	}
 
- // Start configuring the Problem and the Korali Engine
+	// Start configuring the Problem and the Korali Engine
+	Korali::Problem::Posterior problem(heat2DSolver);
+
+	// 3-Candle Model
+	p.nCandles = 3;
+
+	Korali::Parameter::Gaussian pos_x_1("pos_x_1", 0.25, 0.05);
+	Korali::Parameter::Gaussian pos_x_2("pos_x_2", 0.75, 0.05);
+	Korali::Parameter::Gaussian pos_x_3("pos_x_3", 0.75, 0.05);
+
+	pos_x_1.setBounds(0.0, 0.5);
+	pos_x_2.setBounds(0.5, 1.0);
+	pos_x_3.setBounds(0.5, 1.0);
+
+	Korali::Parameter::Uniform pos_y_1("pos_y_1", 0.0, 1.0);
+	Korali::Parameter::Uniform pos_y_2("pos_y_2", 0.0, 1.0);
+	Korali::Parameter::Uniform pos_y_3("pos_y_3", 0.0, 1.0);
+
+	problem.addParameter(&pos_x_1);
+	problem.addParameter(&pos_y_1);
+	problem.addParameter(&pos_x_2);
+	problem.addParameter(&pos_y_2);
+	problem.addParameter(&pos_x_3);
+	problem.addParameter(&pos_y_3);
+
+	problem.setReferenceData(p.nPoints, p.refTemp);
+
+	// Sample distribution
+	Korali::Solver::TMCMC solver(&problem);
+
+	solver.setPopulationSize(10000);
+	solver.setCovarianceScaling(0.2);
+	
+	// Find maximum parameters
+	/*
+	Korali::Solver::CMAES solver(&problem);
+
+	solver.setStopMinDeltaX(1e-11);
+	solver.setPopulationSize(64);
+	solver.setMaxGenerations(1000);
+	*/
+
+	solver.run();
+
+	return 0;
 }
+
