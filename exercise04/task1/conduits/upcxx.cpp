ca375651b3fa2c6b717df05afa4eb9b7297a4cc3
Anian Ruoss <anian.ruoss@gmail.com>
2019-04-24 16:55:54 +0200

    Reformat

--- exercise04/task1/conduits/upcxx.cpp
+++ exercise04/task1/conduits/upcxx.cpp
@@ -18,38 +18,41 @@ std::queue<upcxx::future<size_t>> futures;
 
 void enqueueFuture(size_t rank, size_t sampleId) {
   futures.push(
-          upcxx::rpc(rank, [](size_t sampleId) {
-              return std::make_tuple(rankId, sampleId, 
-             _problem->evaluateSample(&(sampleArrayPointer.local()[sampleId*nParameters])) );
-  }, sampleId).then([](std::tuple<size_t, size_t, double> result) {
-                     _solver->updateEvaluation(std::get<1>(result),
-                                               std::get<2>(result));
-                     return std::get<0>(result);
-                   }));
+      upcxx::rpc(rank,
+                 [](size_t sampleId) {
+                   double fitness = _problem->evaluateSample(
+                       &(sampleArrayPointer.local()[sampleId * nParameters]));
+                   return std::make_tuple(rankId, sampleId, fitness);
+                 },
+                 sampleId)
+          .then([](std::tuple<size_t, size_t, double> result) {
+            _solver->updateEvaluation(std::get<1>(result), std::get<2>(result));
+            return std::get<0>(result);
+          }));
 }
 
 void Korali::Conduit::UPCXX::processSample(size_t sampleId) {
-    
+
   if (futures.size() < rankCount - 1) {
     enqueueFuture(sampleId + 1, sampleId);
   } else {
-      bool done = false;
+    bool done = false;
 
-      do {
-          upcxx::future<size_t> future = futures.front();
-          futures.pop();
+    do {
+      upcxx::future<size_t> future = futures.front();
+      futures.pop();
 
-          upcxx::progress();
+      upcxx::progress();
 
-          if (future.ready()) {
-              size_t rank = future.result();
-              enqueueFuture(rank, sampleId);
-              done = true;
-          } else {
-              futures.push(future);
-          }
+      if (future.ready()) {
+        size_t rank = future.result();
+        enqueueFuture(rank, sampleId);
+        done = true;
+      } else {
+        futures.push(future);
+      }
 
-      } while (!done);
+    } while (!done);
   }
 
   // wait if last sample of generation
@@ -73,8 +76,6 @@ void Korali::Conduit::UPCXX::run() {
     sampleArrayPointer = upcxx::new_array<double>(nSamples * nParameters);
   }
   upcxx::broadcast(&sampleArrayPointer, 1, 0).wait();
-  upcxx::barrier();
-
 
   if (rankId == 0) {
     _solver->runSolver();
