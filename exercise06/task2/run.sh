adaf329d15f7920b2b5c3fe27b0322d66fc84110
Anian Ruoss <anian.ruoss@gmail.com>
2019-05-28 16:31:43 +0200

    Evaluate hybrid model on Euler

---
+++ exercise06/task2/run.sh
@@ -0,0 +1,10 @@
+bsub -R fullnode -n 24 "mpirun -n 24 ./jacobi"
+
+bsub -R fullnode -n 24 "unset LSB_AFFINITY_HOSTFILE; OMP_NUM_THREADS=6 mpirun -n 4 --map-by node:PE=6 --report-bindings ./jacobi_opt"
+bsub -R fullnode -n 48 "unset LSB_AFFINITY_HOSTFILE; OMP_NUM_THREADS=6 mpirun -n 4 --map-by node:PE=6 --report-bindings ./jacobi_opt"
+bsub -R fullnode -n 96 "unset LSB_AFFINITY_HOSTFILE; OMP_NUM_THREADS=6 mpirun -n 4 --map-by node:PE=6 --report-bindings ./jacobi_opt"
+
+bsub -R fullnode -n 24 "unset LSB_AFFINITY_HOSTFILE; OMP_NUM_THREADS=12 mpirun -n 2 --map-by node:PE=12 --report-bindings ./jacobi_opt"
+bsub -R fullnode -n 48 "unset LSB_AFFINITY_HOSTFILE; OMP_NUM_THREADS=12 mpirun -n 4 --map-by node:PE=12 --report-bindings ./jacobi_opt"
+bsub -R fullnode -n 96 "unset LSB_AFFINITY_HOSTFILE; OMP_NUM_THREADS=12 mpirun -n 8 --map-by node:PE=12 --report-bindings ./jacobi_opt"
+
