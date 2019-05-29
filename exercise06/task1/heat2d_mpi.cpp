4e7ba22b9bfe9595afb5a109e0f41e73f8e7bc8a
Anian Ruoss <anian.ruoss@gmail.com>
2019-05-29 22:17:28 +0200

    Parallelize jacobi solver

--- exercise06/task1/heat2d_mpi.cpp
+++ exercise06/task1/heat2d_mpi.cpp
@@ -7,30 +7,168 @@
 
 #include "heat2d_mpi.hpp"
 #include "string.h"
+#include <algorithm>
+#include <cassert>
 #include <chrono>
 #include <limits>
 #include <math.h>
+#include <mpi.h>
 #include <stdio.h>
 
 pointsInfo __p;
 
 int main(int argc, char *argv[]) {
+  MPI_Init(&argc, &argv);
+
+  int myRank, rankCount;
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+  MPI_Comm_size(MPI_COMM_WORLD, &rankCount);
+  bool isMainRank = myRank == 0;
+
   double tolerance =
-      1e-0;      // L2 Difference Tolerance before reaching convergence.
+      1e-0;       // L2 Difference Tolerance before reaching convergence.
   size_t N0 = 10; // 2^N0 + 1 elements per side
+  size_t N = pow(2, N0) - 1; // interior elements per side
 
   // Multigrid parameters -- Find the best configuration!
   size_t gridCount = 1;       // Number of Multigrid levels to use
   size_t downRelaxations = 3; // Number of Relaxations before restriction
   size_t upRelaxations = 3;   // Number of Relaxations after prolongation
 
-  gridLevel *g = generateInitialConditions(N0, gridCount);
+  int dims[2] = {0, 0};
+  MPI_Dims_create(rankCount, 2, dims);
+
+  int periodic[2] = {false, false};
+  MPI_Comm gridComm;
+  MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periodic, false, &gridComm);
+
+  int neighbors[4];
+  MPI_Cart_shift(gridComm, 0, 1, &neighbors[0], &neighbors[1]);
+  MPI_Cart_shift(gridComm, 1, 1, &neighbors[2], &neighbors[3]);
+
+  size_t nx = N / dims[0];
+  size_t ny = N / dims[1];
+
+  // since grid is not divisible by two
+  if (neighbors[1] == MPI_PROC_NULL) {
+    nx = N - (dims[0] - 1) * (N / dims[0]);
+  }
+  if (neighbors[3] == MPI_PROC_NULL) {
+    ny = N - (dims[1] - 1) * (N / dims[1]);
+  }
+
+  size_t fx = nx + 2;
+  size_t fy = ny + 2;
+
+  gridLevel *g = allocateRankGrid(N0, fx, fy, gridCount);
+
+  int requestCountInitialization = 0;
+  MPI_Request requestsInitialization[4 * (rankCount - 1)];
+
+  if (isMainRank) {
+    gridLevel *initialConditions = generateInitialConditions(N0, gridCount);
+    int rank, xStart, xEnd, xLen, yStart, yEnd, yLen;
+    int coords[2];
+
+    for (int xCoord = 0; xCoord < dims[0]; ++xCoord) {
+      for (int yCoord = 0; yCoord < dims[1]; ++yCoord) {
+        coords[0] = xCoord;
+        coords[1] = yCoord;
+        MPI_Cart_rank(gridComm, coords, &rank);
+
+        xStart = xCoord * nx;
+        xEnd = coords[0] == dims[0] - 1 ? N + 2 : (xCoord + 1) * nx + 2;
+        yStart = yCoord * ny;
+        yEnd = coords[1] == dims[1] - 1 ? N + 2 : (yCoord + 1) * ny + 2;
+
+        xLen = xEnd - xStart;
+        yLen = yEnd - yStart;
+
+        if (rank == myRank) {
+          // copy initial data if on same rank
+          for (int i = 0; i < xLen; ++i) {
+            for (int j = 0; j < yLen; ++j) {
+              g[0].U[i * yLen + j] =
+                  initialConditions[0].U[(xStart + i) * g[0].N + yStart + j];
+              g[0].f[i * yLen + j] =
+                  initialConditions[0].f[(xStart + i) * g[0].N + yStart + j];
+            }
+          }
+
+        } else {
+          // send initial data to other ranks
+          double *tmpU = (double *)calloc(xLen * yLen, sizeof(double));
+          double *tmpF = (double *)calloc(xLen * yLen, sizeof(double));
+
+          for (int i = 0; i < xLen; ++i) {
+            for (int j = 0; j < yLen; ++j) {
+              tmpU[i * yLen + j] =
+                  initialConditions[0].U[(xStart + i) * g[0].N + yStart + j];
+              tmpF[i * yLen + j] =
+                  initialConditions[0].f[(xStart + i) * g[0].N + yStart + j];
+            }
+          }
+
+          MPI_Isend(tmpU, xLen * yLen, MPI_DOUBLE, rank, 0, gridComm,
+                    &requestsInitialization[requestCountInitialization++]);
+          MPI_Isend(tmpF, xLen * yLen, MPI_DOUBLE, rank, 1, gridComm,
+                    &requestsInitialization[requestCountInitialization++]);
+        }
+      }
+    }
+
+    freeGrids(initialConditions, gridCount);
+
+  } else {
+    MPI_Irecv(g[0].U, fx * fy, MPI_DOUBLE, 0, 0, gridComm,
+              &requestsInitialization[requestCountInitialization++]);
+    MPI_Irecv(g[0].f, fx * fy, MPI_DOUBLE, 0, 1, gridComm,
+              &requestsInitialization[requestCountInitialization++]);
+  }
+
+  MPI_Waitall(requestCountInitialization, requestsInitialization,
+              MPI_STATUS_IGNORE);
+
+  MPI_Datatype boundaryX, boundaryY;
+  MPI_Type_vector(1, fy, 0, MPI_DOUBLE, &boundaryX);
+  MPI_Type_vector(fx, 1, fy, MPI_DOUBLE, &boundaryY);
+  MPI_Type_commit(&boundaryX);
+  MPI_Type_commit(&boundaryY);
+
+  MPI_Request requests[8];
 
   auto startTime = std::chrono::system_clock::now();
   while (g[0].L2NormDiff > tolerance) // Multigrid solver start
   {
-    applyJacobi(g, 0, downRelaxations); // Relaxing the finest grid first
-    calculateResidual(g, 0);            // Calculating Initial Residual
+    // Relaxing the finest grid first
+    for (size_t r = 0; r < downRelaxations; r++) {
+      applyJacobi(g, 0, fx, fy);
+
+      int request_count = 0;
+
+      MPI_Irecv(&g[0].U[0], 1, boundaryX, neighbors[0], 1, gridComm,
+                &requests[request_count++]);
+      MPI_Irecv(&g[0].U[(fx - 1) * fy], 1, boundaryX, neighbors[1], 1, gridComm,
+                &requests[request_count++]);
+      MPI_Irecv(&g[0].U[0], 1, boundaryY, neighbors[2], 1, gridComm,
+                &requests[request_count++]);
+      MPI_Irecv(&g[0].U[fy - 1], 1, boundaryY, neighbors[3], 1, gridComm,
+                &requests[request_count++]);
+
+      MPI_Isend(&g[0].U[fy], 1, boundaryX, neighbors[0], 1, gridComm,
+                &requests[request_count++]);
+      MPI_Isend(&g[0].U[(fx - 2) * fy], 1, boundaryX, neighbors[1], 1, gridComm,
+                &requests[request_count++]);
+      MPI_Isend(&g[0].U[1], 1, boundaryY, neighbors[2], 1, gridComm,
+                &requests[request_count++]);
+      MPI_Isend(&g[0].U[fy - 2], 1, boundaryY, neighbors[3], 1, gridComm,
+                &requests[request_count++]);
+
+      MPI_Waitall(request_count, requests, MPI_STATUS_IGNORE);
+    }
+
+    // Calculating Initial Residual
+    calculateResidual(g, 0, fx, fy);
 
     // not needed for gridCount == 1
     /*
@@ -41,10 +179,7 @@ int main(int argc, char *argv[]) {
       applyJacobi(g, grid, downRelaxations); // Smoothing coarser level
       calculateResidual(g, grid);            // Calculating Coarse Grid Residual
     }
-    */
 
-    // not needed for gridCount == 1
-    /*
     for (size_t grid = gridCount - 1; grid > 0; grid--) // Going up the V-Cycle
     {
       applyProlongation(
@@ -53,75 +188,107 @@ int main(int argc, char *argv[]) {
     }
     */
 
-    calculateL2Norm(g, 0); // Calculating Residual L2 Norm
-  }                        // Multigrid solver end
+    calculateL2Norm(g, 0, fx, fy); // Calculating Residual L2 Norm
+  }                                // Multigrid solver end
 
   auto endTime = std::chrono::system_clock::now();
   totalTime = std::chrono::duration<double>(endTime - startTime).count();
-  printTimings(gridCount);
-  printf("L2Norm: %.4f\n", g[0].L2Norm);
+
+  for (size_t grid = 0; grid < gridCount; ++grid) {
+    MPI_Reduce(isMainRank ? MPI_IN_PLACE : &smoothingTime[grid],
+               &smoothingTime[grid], 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(isMainRank ? MPI_IN_PLACE : &residualTime[grid],
+               &residualTime[grid], 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(isMainRank ? MPI_IN_PLACE : &restrictionTime[grid],
+               &restrictionTime[grid], 1, MPI_DOUBLE, MPI_MAX, 0,
+               MPI_COMM_WORLD);
+    MPI_Reduce(isMainRank ? MPI_IN_PLACE : &prolongTime[grid],
+               &prolongTime[grid], 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(isMainRank ? MPI_IN_PLACE : &L2NormTime[grid], &L2NormTime[grid],
+               1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(isMainRank ? MPI_IN_PLACE : &totalTime, &totalTime, 1,
+               MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+  }
+
+  if (isMainRank) {
+    printTimings(gridCount);
+    printf("L2Norm: %.4f\n", g[0].L2Norm);
+  }
+
   freeGrids(g, gridCount);
+
+  MPI_Type_free(&boundaryX);
+  MPI_Type_free(&boundaryY);
+  MPI_Comm_free(&gridComm);
+
+  MPI_Finalize();
+
   return 0;
 }
 
-void applyJacobi(gridLevel *g, size_t l, size_t relaxations) {
+void applyJacobi(gridLevel *g, size_t l, size_t fx, size_t fy) {
   auto t0 = std::chrono::system_clock::now();
 
   double h1 = 0.25;
   double h2 = g[l].h * g[l].h;
-  for (size_t r = 0; r < relaxations; r++) {
-    double *tmp = g[l].Un;
-    g[l].Un = g[l].U;
-    g[l].U = tmp;
-    for (size_t i = 1; i < g[l].N - 1; i++)
-      for (size_t j = 1; j < g[l].N - 1; j++) // Perform a Jacobi Iteration
-        g[l].U[i * g[l].N + j] =
-            (g[l].Un[(i - 1) * g[l].N + j] + g[l].Un[(i + 1) * g[l].N + j] +
-             g[l].Un[i * g[l].N + j - 1] + g[l].Un[i * g[l].N + j + 1] +
-             g[l].f[i * g[l].N + j] * h2) *
-            h1;
+
+  double *tmp = g[l].Un;
+  g[l].Un = g[l].U;
+  g[l].U = tmp;
+
+  // Perform a Jacobi Iteration
+  for (size_t i = 1; i < fx - 1; i++) {
+    for (size_t j = 1; j < fy - 1; j++) {
+      g[l].U[i * fy + j] =
+          (g[l].Un[(i - 1) * fy + j] + g[l].Un[(i + 1) * fy + j] +
+           g[l].Un[i * fy + j - 1] + g[l].Un[i * fy + j + 1] +
+           g[l].f[i * fy + j] * h2) *
+          h1;
+    }
   }
 
   auto t1 = std::chrono::system_clock::now();
   smoothingTime[l] += std::chrono::duration<double>(t1 - t0).count();
 }
 
-void calculateResidual(gridLevel *g, size_t l) {
+void calculateResidual(gridLevel *g, size_t l, size_t fx, size_t fy) {
   auto t0 = std::chrono::system_clock::now();
 
   double h2 = 1.0 / pow(g[l].h, 2);
 
-  for (size_t i = 1; i < g[l].N - 1; i++)
-    for (size_t j = 1; j < g[l].N - 1; j++)
-      g[l].Res[i * g[l].N + j] =
-          g[l].f[i * g[l].N + j] +
-          (g[l].U[(i - 1) * g[l].N + j] + g[l].U[(i + 1) * g[l].N + j] -
-           4 * g[l].U[i * g[l].N + j] + g[l].U[i * g[l].N + j - 1] +
-           g[l].U[i * g[l].N + j + 1]) *
+  for (size_t i = 1; i < fx - 1; i++)
+    for (size_t j = 1; j < fy - 1; j++)
+      g[l].Res[i * fy + j] =
+          g[l].f[i * fy + j] +
+          (g[l].U[(i - 1) * fy + j] + g[l].U[(i + 1) * fy + j] -
+           4 * g[l].U[i * fy + j] + g[l].U[i * fy + j - 1] +
+           g[l].U[i * fy + j + 1]) *
               h2;
 
   auto t1 = std::chrono::system_clock::now();
   residualTime[l] += std::chrono::duration<double>(t1 - t0).count();
 }
 
-void calculateL2Norm(gridLevel *g, size_t l) {
+void calculateL2Norm(gridLevel *g, size_t l, size_t fx, size_t fy) {
   auto t0 = std::chrono::system_clock::now();
 
   double tmp = 0.0;
 
-  for (size_t i = 0; i < g[l].N; i++)
-    for (size_t j = 0; j < g[l].N; j++)
-      g[l].Res[i * g[l].N + j] =
-          g[l].Res[i * g[l].N + j] * g[l].Res[i * g[l].N + j];
+  for (size_t i = 1; i < fx - 1; i++)
+    for (size_t j = 1; j < fy - 1; j++)
+      g[l].Res[i * fy + j] = g[l].Res[i * fy + j] * g[l].Res[i * fy + j];
 
-  for (size_t i = 0; i < g[l].N; i++)
-    for (size_t j = 0; j < g[l].N; j++)
-      tmp += g[l].Res[i * g[l].N + j];
+  for (size_t i = 1; i < fx - 1; i++) {
+    for (size_t j = 1; j < fy - 1; j++) {
+      tmp += g[l].Res[i * fy + j];
+    }
+  }
+
+  MPI_Allreduce(MPI_IN_PLACE, &tmp, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
   g[l].L2Norm = sqrt(tmp);
   g[l].L2NormDiff = fabs(g[l].L2NormPrev - g[l].L2Norm);
   g[l].L2NormPrev = g[l].L2Norm;
-  // printf("L2Norm: %.4f\n",  g[0].L2Norm);
 
   auto t1 = std::chrono::system_clock::now();
   L2NormTime[l] += std::chrono::duration<double>(t1 - t0).count();
@@ -185,6 +352,36 @@ void applyProlongation(gridLevel *g, size_t l) {
 }
 */
 
+gridLevel *allocateRankGrid(size_t N0, size_t fx, size_t fy, size_t gridCount) {
+  // only works with gridCount == 1
+  assert(gridCount == 1);
+
+  // Allocating Timers
+  smoothingTime = (double *)calloc(gridCount, sizeof(double));
+  residualTime = (double *)calloc(gridCount, sizeof(double));
+  restrictionTime = (double *)calloc(gridCount, sizeof(double));
+  prolongTime = (double *)calloc(gridCount, sizeof(double));
+  L2NormTime = (double *)calloc(gridCount, sizeof(double));
+
+  // Allocating Grids
+  gridLevel *g = (gridLevel *)malloc(sizeof(gridLevel) * gridCount);
+  for (size_t i = 0; i < gridCount; i++) {
+    g[i].N = pow(2, N0 - i) + 1;
+    g[i].h = 1.0 / (g[i].N - 1);
+
+    g[i].U = (double *)calloc(fx * fy, sizeof(double));
+    g[i].Un = (double *)calloc(fx * fy, sizeof(double));
+    g[i].Res = (double *)calloc(fx * fy, sizeof(double));
+    g[i].f = (double *)calloc(fx * fy, sizeof(double));
+
+    g[i].L2Norm = 0.0;
+    g[i].L2NormPrev = std::numeric_limits<double>::max();
+    g[i].L2NormDiff = std::numeric_limits<double>::max();
+  }
+
+  return g;
+}
+
 gridLevel *generateInitialConditions(size_t N0, size_t gridCount) {
   // Default values:
   __p.nCandles = 4;
@@ -205,13 +402,6 @@ gridLevel *generateInitialConditions(size_t N0, size_t gridCount) {
   pars.push_back(0.754483);
   pars.push_back(0.490461);
   pars.push_back(0.0485152);
-
-  // Allocating Timers
-  smoothingTime = (double *)calloc(gridCount, sizeof(double));
-  residualTime = (double *)calloc(gridCount, sizeof(double));
-  restrictionTime = (double *)calloc(gridCount, sizeof(double));
-  prolongTime = (double *)calloc(gridCount, sizeof(double));
-  L2NormTime = (double *)calloc(gridCount, sizeof(double));
 
   // Allocating Grids
   gridLevel *g = (gridLevel *)malloc(sizeof(gridLevel) * gridCount);
--- exercise06/task1/heat2d_mpi.hpp
+++ exercise06/task1/heat2d_mpi.hpp
@@ -17,10 +17,10 @@ typedef struct gridLevelStruct {
   size_t N;          // Number of points per dimension in the grid level
   double h;          // DeltaX = DeltaY, the distance between points in the
                      // discretized [0,1]x[0,1] domain
-  double *f;        // Right hand side (external heat sources)
-  double *U;        // Main grid
-  double *Un;       // Previous Jacobi grid
-  double *Res;      // Residual Grid
+  double *f;         // Right hand side (external heat sources)
+  double *U;         // Main grid
+  double *Un;        // Previous Jacobi grid
+  double *Res;       // Residual Grid
   double L2Norm;     // L2 Norm of the residual
   double L2NormPrev; // Previous L2 Norm
   double L2NormDiff; // L2Norm Difference compared to previous step
@@ -28,14 +28,15 @@ typedef struct gridLevelStruct {
 
 // Helper Functions
 gridLevel *generateInitialConditions(size_t N0, size_t gridCount);
+gridLevel *allocateRankGrid(size_t N0, size_t fx, size_t fy, size_t gridCount);
 void freeGrids(gridLevel *g, size_t gridCount);
 
 // Solver functions
-void applyJacobi(gridLevel *g, size_t l, size_t relaxations);
-void calculateResidual(gridLevel *g, size_t l);
+void applyJacobi(gridLevel *g, size_t l, size_t fx, size_t fy);
+void calculateResidual(gridLevel *g, size_t l, size_t fx, size_t fy);
 void applyRestriction(gridLevel *g, size_t l);
 void applyProlongation(gridLevel *g, size_t l);
-void calculateL2Norm(gridLevel *g, size_t l);
+void calculateL2Norm(gridLevel *g, size_t l, size_t fx, size_t fy);
 void printTimings(size_t gridCount);
 
 double *smoothingTime;
