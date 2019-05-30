0cba896d830adc5b372dc3ed0eca5c580e36c58d
Anian Ruoss <anian.ruoss@gmail.com>
2019-05-30 02:48:08 +0200

    Evaluate baseline on multiple nodes

--- exercise06/task2/run.sh
+++ exercise06/task2/run.sh
@@ -1,4 +1,6 @@
 bsub -R fullnode -n 24 "mpirun -n 24 ./jacobi"
+bsub -R fullnode -n 48 "mpirun -n 48 ./jacobi"
+bsub -R fullnode -n 96 "mpirun -n 96 ./jacobi"
 
 bsub -R fullnode -n 24 "unset LSB_AFFINITY_HOSTFILE; OMP_NUM_THREADS=6 mpirun -n 4 --map-by node:PE=6 --report-bindings ./jacobi_opt"
 bsub -R fullnode -n 48 "unset LSB_AFFINITY_HOSTFILE; OMP_NUM_THREADS=6 mpirun -n 4 --map-by node:PE=6 --report-bindings ./jacobi_opt"
