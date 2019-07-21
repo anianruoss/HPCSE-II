725eb9e0f020b665d83cfb4c55e38d68854cf75b
Anian Ruoss <anian.ruoss@gmail.com>
2019-07-21 23:08:49 +0200

    Evaluate pdfs on array and use with statement for timer

--- exercise03/importance_sampling.py
+++ exercise03/importance_sampling.py
@@ -3,39 +3,35 @@ import time
 import numpy as np
 import scipy.stats as ss
 
-np.random.seed(0)
 
-n = 10 ** 4
+class Timer:
+    def __enter__(self):
+        self.start = time.perf_counter()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.perf_counter()
+        self.interval = self.end - self.start
+
 
 standard_normal = ss.norm(loc=0, scale=1)
 truncated_exponential = ss.truncexpon(b=np.inf, loc=4.5, scale=1)
 
-# estimator (3)
-start_1 = time.perf_counter()
+for N in np.logspace(4, 8, base=10, num=5, dtype=np.int):
+    # estimator (3)
+    with Timer() as t1:
+        y = standard_normal.rvs(size=N)
+        estimator_1 = np.mean(y > 4.5)
 
-y = standard_normal.rvs(size=n)
+    # estimator (4)
+    with Timer() as t2:
+        x = truncated_exponential.rvs(size=N)
+        f = standard_normal.pdf(x)
+        g = truncated_exponential.pdf(x)
+        estimator_2 = np.mean(f / g)
 
-estimator_1 = (4.5 < y).sum() / n
-
-end_1 = time.perf_counter()
-
-# estimator (3)
-start_2 = time.perf_counter()
-
-x = truncated_exponential.rvs(size=n)
-f = np.array(list(map(standard_normal.pdf, x)))
-g = np.array(list(map(truncated_exponential.pdf, x)))
-
-estimator_2 = (f / g)[4.5 < x].sum() / n
-
-end_2 = time.perf_counter()
-
-print(
-    'Estimator (3):   {:3.10f} [{:2.5f}s]'.format(estimator_1, end_1 - start_1)
-)
-print(
-    'Estimator (4):   {:3.10f} [{:2.5f}s]'.format(estimator_2, end_2 - start_2)
-)
-print(
-    'Exact solution:  {:3.10f}'.format(1. - standard_normal.cdf(4.5))
-)
+    print(f"N = {N}")
+    print(f"Estimator (3):  {estimator_1:.10e} [{t1.interval:.5f}s]")
+    print(f"Estimator (4):  {estimator_2:.10e} [{t2.interval:.5f}s]")
+    print(f"Exact solution: {1. - standard_normal.cdf(4.5):.10e}")
+    print()
